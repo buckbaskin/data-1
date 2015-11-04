5eb5925cdef3f6091a282b95c28247a44902fe30
Buck Baskin <mobile.wbaskin@gmail.com>
2015-11-04 09:28:39 -0500

    Implement DataFloat by duplication from DataInt

--- trace_nums.py
+++ trace_nums.py
@@ -194,6 +194,189 @@ class DataInt(int):
         # TODO(buckbaskin): define what this operation should do
         pass
 
+class DataFloat(float):
+
+    def __new__(cls, *args, **vargs):
+        val = float.__new__(cls, *args, **vargs)
+        if len(args) >= 2:
+            val.__init__(args[0], args[1])
+        else:
+            val.__init__(args[0])
+        val.strong_ref = val
+        return weakref.proxy(val)
+
+    def __init__(self, i_, local_id_tracker=None, source={}):
+        super(DataFloat, self).__init__(i_)
+        
+        self.unique_id = set()
+
+        if local_id_tracker:
+            self.unique_id.update([local_id_tracker.get_id()])
+        else:
+            try:
+                self.unique_id.update([gidt.get_id()])
+            except NameError:
+                raise EnvironmentError('Data Workspace not yet set')
+        self.unique_id.union(source)
+
+    def __repr__(self):
+        return str(self.real)
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __abs__(self):
+        if self.real >= 0:
+            return DataFloat(self.real, source=self.unique_id)
+        else:
+            return DataFloat(-1*self.real, source=self.unique_id)
+
+    def __add__(self, y):
+        return DataFloat(self.real+y.real, 
+            source=self.unique_id.union(y.unique_id))
+
+    def __and__(self, y):
+        return DataFloat(self.real&y.real, 
+            source=self.unique_id.union(y.unique_id))
+
+    def __div__(self, y):
+        # TODO(buckbaskin): check if y is DataFloat, float, and convert
+        return DataFloat(self.real / y.real, 
+            source=self.unique_id.union(y.unique_id))
+
+    # TODO def __coerce__(self, y)
+    # TODO(buckbaskin): implement coerce with more data types
+
+    def __divmod__(self, y):
+        dia = DataFloat(self.real // y.real,
+            source=self.unique_id.union(y.unique_id))
+        dib = DataFloat(self.real % y.real,
+            source=self.unique_id.union(y.unique_id))
+        return (dia, dib,)
+
+    # TODO def __float__(self):
+    # TODO(buckbaskin): implement to DataFloat with more data types
+
+    def __floordiv__(self, y):
+        return DataFloat(self.real // y.real, 
+            source=self.unique_id.union(y.unique_id))
+
+    # TODO def __hash__(self):
+    # TODO(buckbaskin): possibly implement a new hash function
+
+    def __invert__(self):
+        return DataFloat(-1*self.real - 1, 
+            source=self.unique_id.union(y.unique_id))
+
+    # TODO def __long__(self):
+    # TODO(buckbaskin): implement to DataLong with more data types
+
+    def __lshift__(self, y):
+        return DataFloat(self.real << y.real, 
+            source=self.unique_id.union(y.unique_id))
+
+    def __mod__(self, y):
+        return DataFloat(self.real % y.real, 
+            source=self.unique_id.union(y.unique_id))
+
+    def __mul__(self, y):
+        # TODO(buckbaskin): check if y is DataFloat, float, and convert
+        return DataFloat(self.real * y.real, 
+            source=self.unique_id.union(y.unique_id))
+
+    def __neg__(self):
+        return DataFloat(-1*, source=self.unique_id)
+
+    # TODO def __nonzero__(self, y):
+    # TODO(buckbaskin): update this when there is a DataBool
+
+    def __or__(self, y):
+        return DataFloat(self.real | y.real, 
+            source=self.unique_id.union(y.unique_id))
+
+    def __pow__(self, y, z=None):
+        if z is not None:
+            return DataFloat(pow(self.real, y.real, z.real), 
+                source=self.unique_id.union(y.unique_id).union(z.unique_id))
+        else:
+            return DataFloat(pow(self.real, y.real), 
+                source=self.unique_id.union(y.unique_id))
+
+    def __radd__(self, y):
+        return DataFloat(y.real+self.real, 
+            source=self.unique_id.union(y.unique_id))
+
+    def __rand__(self, y):
+        return DataFloat(y.real&self.real, 
+            source=self.unique_id.union(y.unique_id))
+
+    def __rdiv__(self, y):
+        # TODO(buckbaskin): check if y is DataFloat, float, and convert
+        return DataFloat(y.real / self.real, 
+            source=self.unique_id.union(y.unique_id))
+
+    def __rdivmod__(self, y):
+        dia = DataFloat(y.real // self.real, source=self.unique_id.union(y.unique_id))
+        dib = DataFloat(y.real % self.real, source=self.unique_id.union(y.unique_id)))
+        return (dia, dib,)
+
+    def __rfloordiv__(self, y):
+        return DataFloat(y.real // self.real, source=self.unique_id.union(y.unique_id))
+
+    def __rlshift__(self, y):
+        return DataFloat(y.real << self.real, source=self.unique_id.union(y.unique_id))
+
+    def __rmod__(self, y):
+        return DataFloat(y.real % self.real, source=self.unique_id.union(y.unique_id))
+
+    def __rmul__(self, y):
+        # TODO(buckbaskin): check if y is DataFloat, float, and convert
+        return DataFloat(y.real * self.real, source=self.unique_id.union(y.unique_id))
+
+    def __ror__(self, y):
+        return DataFloat(y.real | self.real, source=self.unique_id.union(y.unique_id))
+
+    def __rpow__(self, x, z=None):
+        if z is not None:
+            return DataFloat(pow(x.real, self.real, z.real), source=self.unique_id.union(y.unique_id).union(z.unique_id)))
+        else:
+            return DataFloat(pow(x.real, self.real), source=self.unique_id.union(y.unique_id))
+
+    def __rrshift__(self, y):
+        return DataFloat(y.real >> self.real, source=self.unique_id.union(y.unique_id))
+
+    def __rshift__(self, y):
+        return DataFloat(self.real >> y.real, source=self.unique_id.union(y.unique_id))
+
+    def __rsub__(self, y):
+        return DataFloat(y.real - self.real, source=self.unique_id.union(y.unique_id))
+
+    def __rtruediv__(self, y):
+        return DataFloat(1.0*y.real / self.real, source=self.unique_id.union(y.unique_id))
+
+    def __rxor__(self, y):
+        return DataFloat(y.real^self.real, source=self.unique_id.union(y.unique_id))
+
+    def __sub__(self, y):
+        return DataFloat(self.real - y.real, source=self.unique_id.union(y.unique_id))
+
+    def __truediv__(self, y):
+        return DataFloat(1.0*self.real / y.real, source=self.unique_id.union(y.unique_id))
+
+    def __trunc__(self):
+        return DataFloat(self.real, source=self.unique_id)
+
+    def __xor__(self, y):
+        return DataFloat(self.real^y.real, source=self.unique_id.union(y.unique_id))
+
+    def conjugate(self):
+        return DataFloat(self.real, source=self.unique_id)
+
+    def remove(self):
+        # take steps to remove this data point
+        # TODO(buckbaskin): define what this operation should do
+        pass
+
 def setup_data_workspace():
     global gidt
     gidt = GlobalIdTracker()
