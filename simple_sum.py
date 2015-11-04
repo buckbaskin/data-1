2aaef59020d4f8549a1dde76c1ed381247dbbb11
Buck Baskin <mobile.wbaskin@gmail.com>
2015-11-04 12:16:01 -0500

    New example implementation of student score tracker. Also creates git-visible errors to fix

--- simple_sum.py
+++ simple_sum.py
@@ -1,44 +1,46 @@
-from trace_nums import setup_data_workspace, DataInt
+from trace_nums import setup_data_workspace, DataInt, DataFloat
 from trace_calculation import Calculation
 
 setup_data_workspace()
 
 class StudentAverage(Calculation):
 
-	def __init__(self):
-		super(StudentAverage, self).__init__()
+    def __init__(self):
+        super(StudentAverage, self).__init__()
         self.set_data(0,0)
 
     def set_data(self, score, count):
-    	self.score = score
-    	self.count = count
+        self.score = score
+        self.count = count
 
     def operation(self, new_score):
-    	sa = StudentAverage()
-    	sa.set_data(self.score+new_score, self.count+1)
-    	return sa
+        sa = StudentAverage()
+        sa.set_data(self.score+new_score, self.count+1)
+        return sa
 
     def data_to_value(self):
-    	if count <= 0:
-    		return DataFloat(0.0)
-    	return self.score*1.0/self.count
+        if self.count <= 0:
+            return DataFloat(0.0)
+        return self.score*1.0/self.count
 
 
 
 def main():
-	c = Calculation()
-	c.set_data(1,2,3)
-	print 'c1: '+str(c.data_to_value())
-	dia = DataInt(2)
-	dib = DataInt(3)
-	print 'c2: '+str(c.data_to_value())
-	c.update(dia)
-	print 'c3: '+str(c.data_to_value())
-	c.update(dib)
-	print 'c4: '+str(c.data_to_value())
+    s = StudentAverage()
+    print 's1: '+str(s.data_to_value())
+    
+    studenta = DataInt(75)
+    studentb = DataInt(25)
+    print 's2: '+str(s.data_to_value())
+    
+    s.update(studenta)
+    print 's3: '+str(s.data_to_value())
+    
+    s.update(studentb)
+    print 's4: '+str(s.data_to_value())
 
-	c.reverse(dia.unique_id)
-	print 'c5: '+str(c.data_to_value())
+    s.reverse(studenta.unique_id)
+    print 's5: '+str(s.data_to_value())
 
 if __name__ == '__main__':
-	main()
+    main()
